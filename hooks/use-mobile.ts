e88a79635156e73cb5e3a51ecd9fc37460bf03e6
danixsofyan <danixsofyan@gmail.com>
2026-09-04 06:38:19 +0000

    chore(deps): update to latest patch and minor versions

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -1,19 +1,31 @@
-import * as React from "react"
+import { useSyncExternalStore } from 'react'
 
 const MOBILE_BREAKPOINT = 768
+const QUERY = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
 
-export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+/**
+ * Berlangganan media query lewat `useSyncExternalStore`, bukan `useEffect`
+ * yang memanggil `setState`.
+ *
+ * React 19 menandai pola setState-dalam-effect karena memicu render berantai;
+ * media query adalah sumber data eksternal, dan inilah cara yang disediakan
+ * React untuk membacanya tanpa render tambahan.
+ */
+function subscribe(onChange: () => void): () => void {
+  const mediaQuery = window.matchMedia(QUERY)
+  mediaQuery.addEventListener('change', onChange)
+  return () => mediaQuery.removeEventListener('change', onChange)
+}
 
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
+function getSnapshot(): boolean {
+  return window.matchMedia(QUERY).matches
+}
 
-  return !!isMobile
+/** Server tidak tahu lebar layar; anggap desktop agar tidak salah render. */
+function getServerSnapshot(): boolean {
+  return false
+}
+
+export function useIsMobile(): boolean {
+  return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot)
 }
